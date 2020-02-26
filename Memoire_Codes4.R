ca836dfb6fe6c02cc3dbd1aaa52c1d18a6efa2e4
belangerjulie15 <61424411+belangerjulie15@users.noreply.github.com>
2020-02-26 09:47:56 -0500

    Add files via upload

---
+++ Memoire_Codes4.R
@@ -0,0 +1,229 @@
+############ MÉMOIRE: Maitrise en Mathématiques Finacières et Actuarielle  #################
+
+# -SECTION 4: COMPARAISONS ET PARRALLÉLISATION: Power Utility - Simulation de marché avec frais #
+#-Note: toutes les formules sont UNIQUEMENT applicables à la fonction de Power Utility.
+#Ce dossier fait exactement les mêmes calculs que Memoire_Codes3, mais où tous les 
+#codes ont été parrallélisés pour augmenter la vitesse d'exécution.
+
+
+########## PARAMÈTRES ##########################################
+library(ggplot2)
+library(parallel)
+library(foreach)
+library(iterators)
+library(doParallel)
+
+
+
+Maturi<-10         #Time until maturity
+r_no_risk<-0.02    #Risk free rate 
+alpha<-0.04        #Risky rate
+sigma<-0.2         #Volatility
+gamma<-2           #Parameter of Utility function
+S_0<-1             #Initial value of the asset (S_0>0)
+B_0<-1             #Initial value of the bank account
+budget<-1          #Initial Budget amount
+N_Simulations<-1000 #Number of Simulations
+fee_c_s<-0.005     #Fee applied of the risky asset
+fee_c_f<-0.005     #Fee applied of the funds 
+Frequ<-52          #Frequency of rebalancing the portfolio
+
+a_call_sim<-1      #Multiplicator of the variable annuity
+b_call_sim<-1      #Strike price of the variable annuity
+K_call_sim<-1      #Constant of the variable annuity
+
+alpha_tilde<-alpha-fee_c_s-fee_c_f
+r_no_risk_tilde<-r_no_risk-fee_c_f
+theta_sim<-(alpha-r_no_risk)/sigma
+theta_sim_tilde<-(alpha_tilde-r_no_risk_tilde)/sigma
+
+cte_Merton<-theta_sim_tilde/(sigma*gamma)
+
+numCores_paral <- detectCores() -1
+################################################################
+
+
+######### Directions ############################################
+#  Il faut rouler toute la section FONCTIONS pour activer les fonctions 
+#qu'on veut utiliser.
+
+#Il y a plusieurs sections pour différentes proportions (cte ou dynamiques) investies 
+#dans l'actif risqué.
+################################################################
+
+####### FONCTONS (à rouler)######################################
+
+#1) Payoff d'un call
+payoff_call<-function(X_T,b,a,K){
+  payoff<-a*max(X_T-b,0)+K
+  return(payoff)
+}
+
+#2) Fonction de Power Utility
+P_Utility<-function(X_Tu,gamma){
+  res<-(X_Tu)^(1-gamma)/(1-gamma)
+  return(res)
+}
+
+#3) Trouve x concavifié
+Find_x_theta_PU<-function(b_o,a_o,K_o,gamma_o){
+  f <- function(x) payoff_call(X_T=x,b=b_o,a=a_o,K=K_o)^(-gamma_o)*x*a_o-P_Utility(payoff_call(x,b=b_o,a=a_o,K=K_o),gamma_o)+P_Utility(K_o,gamma_o)        #-Finding the fee
+  result<-uniroot(f, c(b_o,b_o+100))$root
+  return(result)
+}
+
+#4) Trouver le langrangien optimal (pour call bien sûr)
+Find_lambda<-function(r_FL,alpha_FL,sigma_FL,gamma_FL,K_FL,b_FL,a_FL,T_FL,lambda_FL){
+  theta_FL<-(alpha_FL-r_FL)/sigma_FL
+  x_tilde_FL<-Find_x_theta_PU(b_FL,a_FL,K_FL,gamma_FL)
+  indi<--log(a_FL/lambda_FL*(a_FL*(x_tilde_FL-b_FL)+K_FL)^(-gamma_FL))
+  
+  tempo<-(r_FL+0.5*theta_FL^2)*T_FL
+  pro_1<-pnorm((tempo-T_FL*(1-1/gamma_FL)*theta_FL^2-indi)/(theta_FL*sqrt(T_FL)))
+  pro_2<-pnorm((tempo-T_FL*theta_FL^2-indi)/(theta_FL*sqrt(T_FL)))
+  
+  FL_1<-lambda_FL^(-1/gamma_FL)/(a_FL^(1-1/gamma_FL))*pro_1*exp(-tempo*(1-1/gamma_FL)+0.5*T_FL*((1-1/gamma_FL)*theta_FL)^2)
+  FL_2<-(b_FL-K_FL/a_FL)*pro_2*exp(-r_FL*T_FL)
+  
+  X_0<-FL_1+FL_2 
+  return(X_0)
+}
+
+lambda_optimal<-function(r_FLo,alpha_FLo,sigma_FLo,gamma_FLo,K_FLo,b_FLo,a_FLo,X_0_FLo,T_FLo){
+  
+  f <- function(x) Find_lambda(r_FL=r_FLo,alpha_FL=alpha_FLo,sigma_FL=sigma_FLo,gamma_FL=gamma_FLo,K_FL=K_FLo,b_FL=b_FLo,a_FL=a_FLo,T_FL=T_FLo,lambda_FL = x)-X_0_FLo
+  
+  lambda_result<-uniroot(f, c(0.0000000000000001,100))$root  
+  return(lambda_result)
+}
+
+#5) Fonction de tradding à chaque période
+
+# Inputs:
+#invest_risq_tm1-> nombres d'unités dans actif risqué avant le rebalancement au temps t
+#invest_srisq_tm1-> nombre d'unités dans actif sans riqueavant le rebalancement au temps t
+#Outputs:
+#nombres d'unités dans actif risqué/sans risque après le rebalancement.
+Investment_Fonct<-function(S_t,B_t,invest_risq_tm1,invest_srisq_tm1,propor){
+  Ptf_ini<-S_t*invest_risq_tm1+invest_srisq_tm1*B_t
+  
+  investment<-rbind(propor*Ptf_ini/S_t,Ptf_ini*(1-propor)/B_t)
+  
+  return(investment)
+}
+################################################################
+
+
+########### SECTION 0-) SIMULATIONS CONSTANTES POUR COMPARAISON ###########
+
+lambda_opt<-lambda_optimal(r_FL=r_no_risk,alpha_FL=alpha,sigma_FL=sigma,gamma_FL=gamma,K_FL=K_call_sim,b_FL=b_call_sim,a_FL=a_call_sim,X_0_FL=budget,T_FL=Maturi)
+x_concavi<-Find_x_theta_PU(b_call_sim,a_call_sim,K_call_sim,gamma)
+y_concavi<-x_concavi^(-gamma)
+
+random_var<-matrix(rnorm(Maturi*Frequ*N_Simulations),N_Simulations,Maturi*Frequ) #Matrice actif sans risque
+pre1_S_tilde_t<-S_0*exp((alpha_tilde-0.5*sigma^2)/Frequ+sigma*sqrt(1/Frequ)*random_var)
+pre2_S_tilde_t<-cbind((rep(S_0,N_Simulations)),pre1_S_tilde_t)
+
+pre1_B_tilde_t<-rep(exp(r_no_risk_tilde/Frequ),Frequ*Maturi) #vecteur actif sans risque
+B_tilde_t<-B_0*cumprod(c(B_0,pre1_B_tilde_t))
+#########################################################################
+
+
+########### SECTION 1-) "Proportion optimale", avec la méthode martingale ###########
+############################# PARRALÉLISATION ###################################### 
+
+E_utility_martingale_paral<-function(matrice_pre2_S,vecteur_B){
+  #initialisation
+  matrice_S<-matrix(0,N_Simulations,Frequ*Maturi+1)
+  nu_Act_R<-matrix(0,N_Simulations,Frequ*Maturi+1)
+  xi_tilde<-matrix(0,N_Simulations,Frequ*Maturi+1)
+  interior<-matrix(0,N_Simulations,Frequ*Maturi+1)
+  processus_ptf<- matrix(0,N_Simulations,Frequ*Maturi+1)
+  unit_invest<-c(0,0)
+  unit_risque<-matrix(0,N_Simulations,Frequ*Maturi+1)
+  unit_n_risque<-matrix(0,N_Simulations,Frequ*Maturi+1)
+  funds_d<-rep(0,N_Simulations)
+  
+  
+  #Loop sur les simulations parrallélisées
+  loop_parralelized<-function(n){
+    matrice_S[n,]<-cumprod(matrice_pre2_S[n,])
+    
+    for(m in 1:(Frequ*Maturi)){ #Frequ*Maturi
+      petit_t<-(m-1)/(Frequ*Maturi)
+      xi_tilde[n,m]<-matrice_S[n,m]^(-theta_sim_tilde/sigma)*exp((theta_sim_tilde*alpha_tilde/sigma-theta_sim_tilde*sigma/2-r_no_risk_tilde-0.5*theta_sim_tilde^2)*petit_t)
+      
+      interior[n,m]<-((r_no_risk_tilde+(1/gamma-0.5)*theta_sim_tilde^2)*(Maturi-petit_t)+log(y_concavi/(xi_tilde[n,m]*lambda_opt)))/(theta_sim_tilde*sqrt(Maturi-petit_t))
+      nu_Act_R[n,m]<-(theta_sim_tilde/gamma+dnorm(interior[n,m])/(pnorm(interior[n,m])*sqrt(Maturi-petit_t)))/sigma
+      
+      if(m==1){
+        unit_risque[n,m]<-nu_Act_R[n,m]*budget/matrice_S[n,m]
+        unit_n_risque[n,m]<-(budget-unit_risque[n,m]*matrice_S[n,m])/B_tilde_t[m]
+        
+        processus_ptf[n,m]<-budget}
+      else{
+        unit_invest<-Investment_Fonct(matrice_S[n,m],B_tilde_t[m],unit_risque[n,(m-1)], unit_n_risque[n,(m-1)],nu_Act_R[n,m])
+        unit_risque[n,m]<-unit_invest[1]
+        unit_n_risque[n,m]<-unit_invest[2]
+        processus_ptf[n,m]<-unit_risque[n,m]*matrice_S[n,m]+unit_n_risque[n,m]*B_tilde_t[m]}
+      
+    }
+    processus_ptf[n,(Frequ*Maturi+1)]<-unit_risque[n,(Frequ*Maturi)]*matrice_S[n,(Frequ*Maturi+1)]+unit_n_risque[n,(Frequ*Maturi)]*B_tilde_t[(Frequ*Maturi+1)]
+    pre_funds_d<-a_call_sim*max(0,processus_ptf[n,(Frequ*Maturi+1)]-b_call_sim)+K_call_sim
+    return(pre_funds_d)
+  }
+  
+  
+  #registerDoParallel(cores=numCores_paral)
+  #pre1_funds_d<-foreach(i=1:N_Simulations) %dopar% loop_parralelized(i)
+  pre1_funds_d<-mclapply(1:N_Simulations,loop_parralelized, mc.cores = numCores_paral )#(pour MAC)
+  funds_d<-as.numeric(pre1_funds_d)
+  
+  #xi_tilde[n,(Frequ*Maturi+1)]<-matrice_S[n,(Frequ*Maturi+1)]^(-theta_sim_tilde/sigma)*exp((theta_sim_tilde*alpha_tilde/sigma-theta_sim_tilde*sigma/2-r_no_risk_tilde-0.5*theta_sim_tilde^2)*petit_t)
+  #CB<-mean(xi_tilde[n,(Frequ*Maturi+1)]*processus_ptf[n,(Frequ*Maturi+1)])
+  
+  Uty<-as.numeric(lapply(funds_d,function(x)P_Utility(X_Tu=x,gamma=gamma)))
+  EU<-mean(Uty)
+  
+  return(EU)
+}
+
+timer_parrall<-proc.time()
+E_utility_martingale_paral(pre2_S_tilde_t,B_tilde_t)
+proc.time()-timer_parrall
+
+
+########################## TEST ##########################
+
+
+
+
+
+
+cum_sum <- function(p) {
+  retur <- 0
+  for (i in 1:p) {
+    retur <- retur+ i
+  }
+  return(retur)
+}
+
+n <- 20000
+system.time(lapply(1:n, cum_sum))
+#system.time( mclapply(n,cum_sum, mc.cores = 6))
+
+
+registerDoParallel(cores=7)
+as.numeric(foreach(i=1:n) %dopar% cum_sum(i))
+
+cl <- makeCluster(detectCores())
+clusterEvalQ(cl, library(lme4))
+
+
+cl <- makeForkCluster(detectCores()-1)
+clusterSetRNGStream(cl, 1001)
+
+system.time(save3 <- parLapply(cl, 1:n, cum_sum ))
+stopCluster(cl)
+
+
---
+++ Memoire_Codes6.R
@@ -0,0 +1,193 @@
+############ MÉMOIRE: Maitrise en Mathématiques Finacières et Actuarielle  #################
+
+# -SECTION 6: Power Utility - Maximisation de l'utilité pour un groupe d'assurés#
+
+
+######### Directions ############################################
+#-Note: toutes les formules sont UNIQUEMENT applicables à la fonction de Power Utility.
+
+#  Il faut installer/ouvrir le package Optimisation.Power.Utility pour activer les fonctions
+#qu'on veut utiliser.
+
+#Il y a plusieurs sections pour différentes proportions (cte ou dynamiques) investies
+#dans l'actif risqué.
+################################################################
+
+########## PACKAGES ##########################################
+library(ggplot2)
+#library(Optimisation.Power.Utility)
+################################################################
+
+########## PARAMÈTRES ##########################################
+Maturi<-10         #Time until maturity
+r_no_risk<-0.02    #Risk free rate
+alpha<-0.04        #Risky rate
+sigma<-0.2         #Volatility
+gamma<-2           #Parameter of Utility function
+S_0<-1             #Initial value of the asset (S_0>0)
+B_0<-1             #Initial value of the bank account
+budget<-1          #Initial Budget amount
+N_Simulations<-1000 #Number of Simulations
+fee_c_s<-0.005     #Fee applied of the risky asset
+fee_c_f<-0.005     #Fee applied of the funds
+Frequ<-52          #Frequency of rebalancing the portfolio
+
+a_call_sim<-1      #Multiplicator of the variable annuity
+b_call_sim<-1      #Strike price of the variable annuity
+K_call_sim<-1      #Constant of the variable annuity
+
+alpha_tilde<-alpha-fee_c_s-fee_c_f
+r_no_risk_tilde<-r_no_risk-fee_c_f
+theta_sim<-(alpha-r_no_risk)/sigma
+theta_sim_tilde<-(alpha_tilde-r_no_risk_tilde)/sigma
+
+cte_Merton<-theta_sim_tilde/(sigma*gamma)
+################################################################
+
+########### Fonctions du packages Optimisation.Power.Utility ###########
+
+payoff_call<-function(X_T,b,a,K){
+  # X_T: valeur du stock à maturité
+  # b: Strike value
+  # K: valeur initiale du Payoff
+
+  # Call de la forme:  a*max(X_T-b,0)+K
+  payoff<-a*max(X_T-b,0)+K
+  return(payoff)
+}
+payoff_call(10,5,0.5,8)
+
+P_Utility<-function(X_Tu,gamma){
+  res<-(X_Tu)^(1-gamma)/(1-gamma)
+  return(res)
+}
+
+Find_x_theta_PU<-function(b_o,a_o,K_o,gamma_o){
+  f <- function(x) payoff_call(X_T=x,b=b_o,a=a_o,K=K_o)^(-gamma_o)*x*a_o-P_Utility(payoff_call(x,b=b_o,a=a_o,K=K_o),gamma_o)+P_Utility(K_o,gamma_o)        #-Finding the fee
+  result<-uniroot(f, c(b_o,b_o+100))$root
+  return(result)
+}
+Find_x_theta_PU(b_o=30,a_o=0.5,K_o=15,gamma_o=4)
+
+P_Utility_con<-function(X_T_con,gamma_c,b_c,a_c,K_c){
+  x_theta<-Find_x_theta_PU(b_o=b_c,a_o=a_c,K_o=K_c,gamma_o=gamma_c)
+  slope<-(P_Utility(payoff_call(x_theta,b_c,a_c,K_c),gamma_c)-P_Utility(K_c,gamma_c))/(x_theta)
+
+  if(X_T_con<=x_theta){Chord<-P_Utility(K_c,gamma_c)+X_T_con*slope}
+  else{Chord<-P_Utility(payoff_call(X_T=X_T_con,b=b_c,a=a_c,K=K_c),gamma_c)}
+
+  return(Chord)
+}
+
+
+Derivee_P_Utility_con<-function(X_T_con,gamma_c,b_c,a_c,K_c){
+  x_theta<-Find_x_theta_PU(b_o=b_c,a_o=a_c,K_o=K_c,gamma_o=gamma_c)
+  slope<-(P_Utility(payoff_call(x_theta,b_c,a_c,K_c),gamma_c)-P_Utility(K_c,gamma_c))/(x_theta)
+
+  if(X_T_con<=x_theta){Chord<-slope}
+  else{Chord<-payoff_call(X_T=X_T_con,b=b_c,a=a_c,K=K_c)^(-gamma_c)*a_c}
+
+  return(Chord)
+}
+
+Convexe_Fun<-function(element){ #Fonction convexe
+  res<-(element)^5
+  return(res)
+}
+
+droite<-function(pente, origine,val_x){
+  return(origine+val_x*pente)
+}
+
+find_roots<-function(pente1,origine1,beg,end){
+  f <- function(x) droite(pente1,origine1,x)-Convexe_Fun(x)
+  result1<-uniroot(f, c(beg,end))$root
+  return(result1)
+}
+find_roots(15000,-20000,0,4)
+
+X_opt_given_S<-function(S_T,gamma,sigma,alpha,r){
+  result<-(S_T*gamma*sigma^2)/(alpha-r)
+  return(result)
+}
+
+CRRA<-function(x,gamma){
+  result<-(x^(1-gamma)-1)/(1-gamma)
+  return(result)
+}
+
+PowerUtility_2<-function(gamma1,gamma2,x){
+  sumUtl<-P_Utility(x,gamma1)+P_Utility(x,gamma2)
+  return(sumUtl)
+}
+
+PowerUtility_3<-function(gamma1,gamma2,gamma3,x){
+  sumUtl<-P_Utility(x,gamma1)+P_Utility(x,gamma2)+P_Utility(x,gamma3)
+  return(sumUtl)
+}
+
+PowerUtility_con_2<-function(gamma1,gamma2,b,a,K,x){
+  sumUtl<-P_Utility_con(x, gamma1, b, a, K)+P_Utility_con(x, gamma2, b, a, K)
+  return(sumUtl)
+}
+
+PowerUtility_con_3<-function(gamma1,gamma2,gamma3,b,a,K,x){
+  sumUtl<-P_Utility_con(x, gamma1, b, a, K)+P_Utility_con(x, gamma2, b, a, K)++P_Utility_con(x, gamma3, b, a, K)
+  return(sumUtl)
+}
+
+Find_x_theta_PU_Numerous<-function(b_o,a_o,K_o,vector_gamma){
+
+  f <- function(x)  sum(P_Utility(payoff_call(x,b_o,a_o,K_o),vector_gamma))-sum(P_Utility(K_o,vector_gamma))-x*sum((x)^(-vector_gamma))
+  sln_x<-uniroot(f, c(b_o,b_o+100))$root
+
+  return(sln_x)
+}#Verification (dans fichier function, Optimisation.Power.Utility) faite.
+
+
+#####################################################
+
+
+########### SECTION 0-) SIMULATIONS CONSTANTES POUR COMPARAISON ###########
+
+lambda_opt<-lambda_optimal(r_FL=r_no_risk,alpha_FL=alpha,sigma_FL=sigma,gamma_FL=gamma,K_FL=K_call_sim,b_FL=b_call_sim,a_FL=a_call_sim,X_0_FL=budget,T_FL=Maturi)
+x_concavi<-Find_x_theta_PU(b_call_sim,a_call_sim,K_call_sim,gamma)
+y_concavi<-x_concavi^(-gamma)
+
+random_var<-matrix(rnorm(Maturi*Frequ*N_Simulations),N_Simulations,Maturi*Frequ) #Matrice actif sans risque
+pre1_S_tilde_t<-S_0*exp((alpha_tilde-0.5*sigma^2)/Frequ+sigma*sqrt(1/Frequ)*random_var)
+pre2_S_tilde_t<-cbind((rep(S_0,N_Simulations)),pre1_S_tilde_t)
+
+pre1_xi_tilde_t<-exp(-(r_no_risk_tilde+0.5*theta_sim_tilde^2)/Frequ-theta_sim_tilde*sqrt(1/Frequ)*random_var)
+pre2_xi_tilde_t<-cbind((rep(S_0,N_Simulations)),pre1_xi_tilde_t)
+
+pre1_B_tilde_t<-rep(exp(r_no_risk_tilde/Frequ),Frequ*Maturi) #vecteur actif sans risque
+B_tilde_t<-B_0*cumprod(c(B_0,pre1_B_tilde_t))
+#########################################################################
+
+
+######### Somme d'utilités: Comparaison individuel VS en groupe ############
+
+test<-seq(0.7,2.5,0.05)
+test_call<-as.numeric(lapply(test,function(x)payoff_call(x,b=1,a=1,K=1)))
+
+plot(test,lapply(test,function(x) PowerUtility_2(2,3,x)),type='l',ylim = c(-2,0),ylab='',col='blue')
+lines(test,lapply(test,function(x) P_Utility(x,2)))
+lines(test,lapply(test,function(x) P_Utility(x,3)))
+
+plot(test,lapply(test_call,function(x) PowerUtility_2(2,3,x)),type='l',ylim = c(-2,0),ylab='',col='blue')
+lines(test,lapply(test_call,function(x) P_Utility(x,2)))
+lines(test,lapply(test_call,function(x) P_Utility(x,3)))
+
+plot(test,lapply(test_call,function(x) PowerUtility_3(2,3,5,x)),xaxt = "n",yaxt = "n",type='l',ylim = c(-2,0),ylab = "Utilité",xlab=expression(paste("Valeur de l'actif à maturité (x)" )),col='blue',xlim = c(0.8,2.7))
+lines(test,lapply(test_call,function(x) P_Utility(x,2)),lty=2)
+lines(test,lapply(test_call,function(x) P_Utility(x,3)),lty=4)
+lines(test,lapply(test_call,function(x) P_Utility(x,5)),lty=5)
+xtick<-c(b=b_call_sim,round(Find_x_theta_PU(b_o=b_call_sim,a_o=a_call_sim,K_o=K_call_sim,gamma_o=3),4),round(Find_x_theta_PU(b_o=b_call_sim,a_o=a_call_sim,K_o=K_call_sim,gamma_o=2),4),round(Find_x_theta_PU(b_o=b_call_sim,a_o=a_call_sim,K_o=K_call_sim,gamma_o=5),4),round(Find_x_theta_PU_Numerous(b_call_sim,a_call_sim,K_call_sim,c(2,3,5)),4))
+axis(1, at=xtick, labels=c(expression(D[T]),expression(paste(tilde(x),"(",gamma[3],")")),expression(paste(tilde(x),"(",gamma[2],")")),expression(paste(tilde(x),"(",gamma[5],")")),''))
+text(2.65, -0.38, expression(paste(gamma,'=2')))
+text(2.65, -0.1, expression(paste(gamma,'=3')))
+text(2.65, -0.001, expression(paste(gamma,'=5')))
+text(2.65, -0.49, 'Totale')
+abline(v=Find_x_theta_PU_Numerous(b_call_sim,a_call_sim,K_call_sim,c(2,3,5)),  col ='blue',lty=3)
+
