0529005adbffd291c4abf271fd25f70a33fd8b82
Julie Bélanger <annieclaudebelanger@MacBook-Pro-de-Annie-4.local>
2020-03-17 18:29:57 -0400

    Graphe: fonction convexe

--- Memoire_Codes.R
+++ Memoire_Codes.R
@@ -188,6 +188,27 @@ ytick<-Derivee_P_Utility_con(0,gamma_c=par_Gamma,b_c=par_b,a_c=par_a,K_c=par_K)
 axis(1, at=xtick, labels=c(0 ,expression(tilde(x))))
 axis(2, at=ytick, labels=c( expression(paste(tilde(u)," '",(tilde(x))))))
 
+#Graphe: Fonction convexe#
+x_1<-1.5
+x_2<-3.5
+
+axe_d_x<-seq(1,4,0.05)
+plot(axe_d_x,lapply(axe_d_x,function(x)x^5.5), type='l',ylim=c(-200,2048),xaxt = "n",yaxt = "n",xlab = '',ylab='')
+xtick<-c(x_1,x_2)
+ytick<-c(lapply(x_1,function(x)x^5.5),lapply(x_2,function(x)x^5.5))
+axis(1, at=xtick, labels=c(expression(x[1]),expression(x[2])),las=0,col='azure4')
+axis(2, at=ytick, labels=c( expression(paste('f(',x[1],')')),expression(paste('f(',x[2],')'))),las=1,col='azure4')
+
+segments(x0=x_1, y0=-200, x1 =x_1, y1 =as.numeric(ytick[1]), lty=3, col='azure4')
+segments(x0=x_2, y0=-200, x1 =x_2, y1 =as.numeric(ytick[2]), lty=3, col='azure4')
+segments(x0=0, y0=as.numeric(ytick[1]), x1 =x_1, y1 =as.numeric(ytick[1]), lty=3, col='azure4')
+segments(x0=0, y0=as.numeric(ytick[2]), x1 =x_2, y1 =as.numeric(ytick[2]), lty=3, col='azure4')
+segments(x0=x_1, y0=as.numeric(ytick[1]), x1 =x_2, y1 =as.numeric(ytick[2]), lty=4, col='red')
+
+
+
+
+
 
 ##### -TEST SECTION- #####
 
